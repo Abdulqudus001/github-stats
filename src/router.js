7d36315a6126d538c657531be0b2482f16c7e7f7
abdulqudus001 <abdulqudusabubakre@gmail.com>
2020-02-17 09:58:17 +0100

    Configured vue router

--- src/router.js
+++ src/router.js
@@ -1,2 +1,11 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router'
+
+// Importing components
+import Home from '@/components/Home.vue';
+
+const routes = [
+  { path: '/home', component: Home }
+]
+
+module.exports = new VueRouter({routes})
