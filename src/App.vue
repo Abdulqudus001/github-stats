03ba1843d9346928d88f9421c3e4c7b2ac973320
abdulqudus001 <abdulqudusabubakre@gmail.com>
2020-02-17 16:31:25 +0100

    Setup vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,97 @@
 <template>
-  <div id="app">
-    <router-view></router-view>
-  </div>
+  <v-app id="inspire">
+    <v-app-bar
+      app
+      clipped-left
+      color="blue-grey"
+      dark
+    >
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <v-toolbar-title>GitHub Stats</v-toolbar-title>
+      <v-spacer />
+    </v-app-bar>
+
+    <v-navigation-drawer
+      v-model="drawer"
+      color="blue-grey"
+      app
+      clipped
+    >
+      <v-list dense>
+        <v-list-item @click.stop="left = !left">
+          <v-list-item-action>
+            <v-icon>mdi-exit-to-app</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Open Temporary Drawer</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-navigation-drawer
+      v-model="left"
+      fixed
+      temporary
+    />
+
+    <v-content>
+      <v-container
+        class="fill-height"
+        fluid
+      >
+        <v-row
+          align="center"
+          justify="center"
+        >
+          <v-col
+            cols="12"
+            sm="8"
+          >
+            <router-view></router-view>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-content>
+
+    <v-navigation-drawer
+      v-model="right"
+      fixed
+      right
+      temporary
+    />
+
+    <v-footer
+      app
+      color="blue-grey"
+      class="white--text"
+    >
+      <span>Made with by Abdulqudus</span>
+      <v-spacer />
+      <span>&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
 </template>
 
 <script>
-
-export default {
-  name: 'App',
-};
+  export default {
+    props: {
+      source: String,
+    },
+    data: () => ({
+      drawer: null,
+      drawerRight: null,
+      right: false,
+      left: false,
+    }),
+  };
 </script>
 
 <style src="vue-multiselect/dist/vue-multiselect.min.css"></style>
 <style>
   @import "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css";
 </style>
+
 <style>
 body {
   background: #2c3e50;
@@ -29,6 +106,10 @@ body {
   max-width: calc(100% - 30px);
   padding: 0 15px;
   margin: 50px auto;
+}
+
+.theme--light.v-application {
+  background: none !important;
 }
 </style>
 
