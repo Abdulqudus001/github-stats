c3bbbeeec39717a309b1bcbff3b16c03682632c7
abdulqudus001 <abdulqudusabubakre@gmail.com>
2020-02-17 16:51:32 +0100

    Setup navbar

--- src/App.vue
+++ src/App.vue
@@ -18,12 +18,19 @@
       clipped
     >
       <v-list dense>
-        <v-list-item @click.stop="left = !left">
+        <v-list-item
+          v-for="item in items"
+          :key="item.text"
+          link
+          :to="item.to"
+        >
           <v-list-item-action>
-            <v-icon>mdi-exit-to-app</v-icon>
+            <v-icon>mdi-{{ item.icon }}</v-icon>
           </v-list-item-action>
           <v-list-item-content>
-            <v-list-item-title>Open Temporary Drawer</v-list-item-title>
+            <v-list-item-title>
+              {{ item.text }}
+            </v-list-item-title>
           </v-list-item-content>
         </v-list-item>
       </v-list>
@@ -66,7 +73,7 @@
       color="blue-grey"
       class="white--text"
     >
-      <span>Made with by Abdulqudus</span>
+      <span>Made with by <v-icon color="pink" class="pound">mdi-heart</v-icon><a href="github.com/abdulqudus001">Abdulqudus</a></span>
       <v-spacer />
       <span>&copy; {{ new Date().getFullYear() }}</span>
     </v-footer>
@@ -83,6 +90,13 @@
       drawerRight: null,
       right: false,
       left: false,
+      items: [
+        { icon: 'home', text: 'Home', to: '/' },
+        { icon: 'chart-line', text: 'Visualize', to: '/stats' },
+        { icon: 'history', text: 'Another route', to: '/' },
+        { icon: 'featured-play-list', text: 'Another one', to: '/' },
+        { icon: 'watch-later', text: 'Final one', to: '/' },
+      ],
     }),
   };
 </script>
@@ -111,6 +125,33 @@ body {
 .theme--light.v-application {
   background: none !important;
 }
+.pound + a {
+  text-decoration: none;
+  color: #fff !important;
+}
+
+.pound + a:hover {
+  color: #ddd !important;
+}
+
+.pound {
+  margin: 0 3px;
+  font-size: 10px;
+  animation: pound 0.35s infinite alternate;
+  -webkit-animation: pound 0.35s infinite alternate;
+}
+
+@-webkit-keyframes pound {
+  to {
+    transform: scale(1.1);
+  }
+}
+@keyframes pound {
+  to {
+    transform: scale(1.1);
+  }
+}
+
 </style>
 
 <style lang="scss">
